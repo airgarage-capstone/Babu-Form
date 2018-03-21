9522d19b2212001a0bfe9f96dde7215e6ce25c92
babu406 <36079997+babu406@users.noreply.github.com>
2018-03-21 09:50:54 -0700

    Add files via upload

---
+++ Cards/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+import Info	from './Info';
+import Cards from './Cards';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+		
+		<h1> </h1>
+		<Cards></Cards>
+      </div>
+    );
+  }
+}
+
+export default App;
+ <h1>Welcome To AirGarage</h1>
