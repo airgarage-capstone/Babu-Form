2b6def4f0c9c6ee07793934c8d689e0b30d53812
babu406 <36079997+babu406@users.noreply.github.com>
2018-03-28 01:53:10 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import axios from 'axios';
 import './App.css';
 import Form from './Form';
 import Info	from './Info';
 import Cards from './Cards';
 
 class App extends Component {
-  
-  state = {
-		cardSpots:[]
-		
-	}
-  
-  componentDidMount(){
-	axios.get('https://airgara.ge/api/spots/')
-	.then(res => {
-			const cardSpots = res.data;
-			this.setState({cardSpots});
-		
-	})
-  }
-  
   render() {
     return (
       <div className="App">
-        <Info></Info>
-		<Form></Form>
-		<br1></br1>
-		{this.state.cardSpots.map(cardSpot => <Cards
-			
-			Spotname = {cardSpot.name}
-			address = {cardSpot.address.address_line1}
-			city = {cardSpot.address.city}
-			state = {cardSpot.address.state}
-			pricePerMonth = {cardSpot.price}
-			allowAutoBook = {cardSpot.auto_book}
-			quantity = {cardSpot.quantity}
-			
-		/>)}
+        
 		
+		<h1> </h1>
+		<Cards></Cards>
       </div>
     );
   }
